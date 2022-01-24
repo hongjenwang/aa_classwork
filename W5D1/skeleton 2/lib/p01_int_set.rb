2f56fd9282e5fd15ef2a62443f05988a480cd1f7
anahmmacher <acnahmmacher@gmail.com>
2022-01-24 15:26:30 -0800

    Adding IntSet class

--- W5D1/skeleton 2/lib/p01_int_set.rb
+++ W5D1/skeleton 2/lib/p01_int_set.rb
@@ -30,23 +30,30 @@ end
 
 
 class IntSet
+  attr_reader :store
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
   end
 
   def insert(num)
+    self[num].push(num)
   end
 
   def remove(num)
+    if self.include?(num)
+      c = self[num].index(num)
+      self[num].slice!(c)
+    end
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    self.store[num % num_buckets]
   end
 
   def num_buckets
